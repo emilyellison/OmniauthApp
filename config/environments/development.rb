a68dd89f2434245f85c484f9ccabe5bf375a2188
Emily Ellison <emilyellison986@gmail.com>
2012-07-12 10:19:05 -0500

    Set Up Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,15 @@ OmniauthApp::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+    
+  # Ensure you have defined default url options in your environments files. Here 
+  # is an example of default_url_options appropriate for a development environment 
+  # in config/environments/development.rb:
+  # 
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # 
+  # In production, :host should be set to the actual host of your application.
+  
+  # Devise told me to add configuration for my action mailer.
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
