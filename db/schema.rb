9b9ab579dada96bcde9296670f13bf9c8897e0a7
Emily Ellison <emilyellison986@gmail.com>
2012-07-12 10:27:07 -0500

    Add Authentication Model & Connect it to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120712151845) do
+ActiveRecord::Schema.define(:version => 20120712152532) do
+
+  create_table "authentications", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
