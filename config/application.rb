e87f28a49b88f9083b62251e2510f24eeac2d73e
Emily Ellison <emilyellison986@gmail.com>
2012-07-12 10:21:45 -0500

    Set Up Devise Host

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module OmniauthApp
     
     # Based on Devise instructions.
     config.assets.initialize_on_precompile = false
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
